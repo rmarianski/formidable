3bed8ca0192ae24ca946e18bff0a27a9e2d24c7b
Robert Marianski <rob@marianski.com>
2011-05-18 01:16:48 -0400

    beginning of uniform compatible form generation

--- src/formidable/core.clj
+++ src/formidable/core.clj
@@ -15,29 +15,28 @@
            :name (:name field "submit")
            :value (:value field "submit")}])
 
-(defmethod render :form
+(defmethod render :uniform
   [form params]
   (if-let [fields (:fields form)]
-    [:form {:method (:method form :post) :action (:action form)}
+    [:form.uniForm {:method (:method form :post) :action (:action form)}
      (when-let [label (:label form)] [:h2 label])
      (when-let [description (:description form)] [:p description])
-     (for [field fields]
-       (let [field-name (:name field)
-             error-message (:error field)
-             required (:required field true)
-             renderer (:renderer field render)
-             value (get (or params {}) field-name)]
-         ;; maybe custom layout function?
-         [:div
-          (when error-message [:div.error error-message])
-          (when required [:span.required])
-          (let [label (:label field (capitalize (as-str field-name)))
-                description (:description field)]
-            (list
-             [:label label]
-             (when description [:p description])
-             (renderer field value)))]))
-     [:div (map #(render % params) (:buttons form))]]
+     [:fieldset
+      (for [field fields]
+        (let [field-name (:name field)
+              error-message (:error field)
+              required (:required field true)
+              renderer (:renderer field render)
+              value (get (or params {}) field-name)]
+          [:div.ctrlHolder
+           (let [label (:label field (capitalize (as-str field-name)))
+                 description (:description field)]
+             (list
+              [:label label (when required [:span.required "*"])]
+              (renderer field value)
+              (when description [:p.formHint description])
+              (when error-message [:div.error error-message])))]))
+      [:div.buttonHolder (map #(render % params) (:buttons form))]]]
      (throw (IllegalArgumentException. "render form: no fields"))))
 
 (defmethod render nil [_ _] nil)
