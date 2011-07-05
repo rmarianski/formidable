302ffcb4ee0f26e718d28e2b941e43b78f658958
Robert Marianski <rob@marianski.com>
2011-07-05 15:06:39 -0400

    initial crack at building on top of flutter

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject formidable "0.1-SNAPSHOT"
   :description "simple form toolkit"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [flutter "0.0.2"]])
--- src/formidable/core.clj
+++ src/formidable/core.clj
@@ -1,42 +1,53 @@
 (ns formidable.core
   (:use [clojure.string :only (capitalize)]
-        [clojure.contrib.string :only (as-str)]
-        [clojure.contrib.core :only (seqable?)]))
+        [clojure.contrib.string :only (as-str)]))
 
-(defmulti render :type)
+; uniform specific
+(defn wrap-form-field
+  "wraps a particular field with a label, description, error which is
+  fetched from opts which must be a map. options for the field itself
+  go under the :opts key"
+  [f]
+  (fn [type attrs name opts value]
+    [:div.ctrlHolder
+     (list
+      [:label {:required (:required opts)}
+       (or (:label opts) (capitalize (as-str name)))]
+      (when-let [desc (:description opts)]
+        [:p.formHint desc])
+      (f type attrs name (:opts opts) value)
+      (when-let [error (:error opts)]
+        [:div.error error]))]))
 
-(defmethod render :text
-  [field value]
-  [:input {:type :text :name (:name field) :value (or value "")}])
+(defn wrap-errors
+  "add in errors for form fields puts the error in opts if it's a map"
+  [f errors]
+  (fn [type attrs name opts value]
+    (if (nil? opts)
+      (recur type attrs name {} value)
+      (f type attrs name
+         (if (map? opts)
+           (merge {:error (errors name)} opts)
+           opts)
+         value))))
 
-(defmethod render :submit
-  [field _]
-  [:input {:type :submit
-           :name (:name field "submit")
-           :value (:value field "submit")}])
-
-(defmethod render :uniform
-  [form params]
-  (if-let [fields (:fields form)]
-    [:form.uniForm {:method (:method form :post) :action (:action form)}
-     (when-let [label (:label form)] [:h2 label])
-     (when-let [description (:description form)] [:p description])
-     [:fieldset
-      (for [field fields]
-        (let [field-name (:name field)
-              error-message (:error field)
-              required (:required field true)
-              renderer (:renderer field render)
-              value (get (or params {}) field-name)]
-          [:div.ctrlHolder
-           (let [label (:label field (capitalize (as-str field-name)))
-                 description (:description field)]
-             (list
-              [:label label (when required [:span.required "*"])]
-              (renderer field value)
-              (when description [:p.formHint description])
-              (when error-message [:div.error error-message])))]))
-      [:div.buttonHolder (map #(render % params) (:buttons form))]]]
-     (throw (IllegalArgumentException. "render form: no fields"))))
-
-(defmethod render nil [_ _] nil)
+  ;; [:form.uniForm {:method (:method form-opts :post) :action (:action form-opts)}
+  ;;    (when-let [label (:label form)] [:h2 label])
+  ;;    (when-let [description (:description form)] [:p description])
+  ;;    [:fieldset
+  ;;     (for [field fields]
+  ;;       (let [field-name (:name field)
+  ;;             error-message (:error field)
+  ;;             required (:required field true)
+  ;;             renderer (:renderer field render)
+  ;;             value (get (or params {}) field-name)]
+  ;;         [:div.ctrlHolder
+  ;;          (let [label (:label field (capitalize (as-str field-name)))
+  ;;                description (:description field)]
+  ;;            (list
+  ;;             [:label label (when required [:span.required "*"])]
+  ;;             (renderer field value)
+  ;;             (when description [:p.formHint description])
+  ;;             (when error-message [:div.error error-message])))]))
+  ;;     [:div.buttonHolder (map #(render % params) (:buttons form))]]]
+  ;;    (throw (IllegalArgumentException. "render form: no fields"))))
